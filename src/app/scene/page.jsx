08119eaefc57a35df3692bd3be7f718d6e1675ff
Nishit Sarvaiya <nishit@darwindigital.com>
2024-06-14 15:25:14 +0530

    minor build fix

--- src/app/scene/page.jsx
+++ src/app/scene/page.jsx
@@ -1,5 +1,10 @@
 import Scene from "@/components/Scene/Scene";
+import { Suspense } from "react";
 
 export default function page() {
-	return <Scene />;
+	return (
+		<Suspense>
+			<Scene />
+		</Suspense>
+	);
 }
